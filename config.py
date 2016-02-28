3eb7e0fcc38cda2c56f5249b83daadec9d79048d
akshay bhat <akshayubhat@gmail.com>
2016-02-28 01:12:18 -0500

    frame extraction

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import sys
 BUCKETNAME = "aub3cardata"
 PREFIX = "test"
 USER = "ubuntu"
-HOST = "54.85.205.156"
+HOST = "52.90.249.222"
 private_key =  "~/.ssh/cs5356"
 CONFIG_PATH = __file__.split('settings.py')[0]
 AWS = sys.platform != 'darwin'
--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 import os,logging
-from fabric.api import task,local,run
+from fabric.state import env
+from fabric.api import task,local,run,put,get,lcd,cd,sudo
 import boto,time
 from boto.s3.key import Key
 logging.basicConfig(level=logging.INFO,
@@ -9,6 +10,9 @@ logging.basicConfig(level=logging.INFO,
                     filemode='a')
 import config
 from config import USER,private_key,HOST
+env.user = USER
+env.key_filename = private_key
+env.hosts = [HOST,]
 
 s3_connection = boto.connect_s3()
 
@@ -17,15 +21,13 @@ def get_frames():
     bucket = s3_connection.get_bucket(config.BUCKETNAME)
     videos = [line.strip().split(" ")[-1] for line in file("lib/videos.txt") if line.strip().endswith(".mp4")]
     for i,v in enumerate(videos):
-        if i > 2:
-            key = bucket.get_key(v)
-            name = v.split("/")[-1].split(".")[0]
-            url = key.generate_url(expires_in=600)
-            time.sleep(5)
-            command = 'ffmpeg -i "{}" -vf fps=1/60 temp/{}.%04d.png'.format(url,name)
-            print command
-            os.system(command)
-            break
+        key = bucket.get_key(v)
+        name = v.split("/")[-1].split(".")[0]
+        url = key.generate_url(expires_in=600)
+        time.sleep(5)
+        command = 'ffmpeg -ss 60 -to 600 -i "{}" -vf fps=1/60 temp/{}.%04d.png'.format(url,name)
+        print command
+        os.system(command)
 
 
 
@@ -38,3 +40,14 @@ def server(rlocal=False):
         local('python server.py')
     else:
         run('python server.py')
+
+@task
+def connect():
+    """
+    Creates connect.sh for the current host
+    :return:
+    """
+    fh = open("connect.sh",'w')
+    fh.write("#!/bin/bash\n"+"ssh -i "+env.key_filename+" "+"ubuntu"+"@"+HOST+"\n")
+    fh.close()
+
